95c937830babe932cc59324a6e1801955c0cc43d
BKLP22 <97519892+BKLP22@users.noreply.github.com>
2024-02-26 20:39:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [player1, setPlayer1] = useState('');
+  const [player2, setPlayer2] = useState('');
+  const [result, setResult] = useState('');
+
+  const playGame = async () => {
+    try {
+      if (player1 === "" || player2 === "") {
+        setResult("Ambos jugadores han de escoger")
+      } else {
+        const backendUrl = "http://localhost:5265";
+        const queryParameters = new URLSearchParams({ player1, player2 }).toString();
+        const response = await axios.post(`${backendUrl}/api/Game?${queryParameters}`);
+        setResult(response.data);
+      }
+    } catch (error) {
+      console.error('Error con la partida:', error);
+    }
+  };
+
+  const cambiarOpcionJugador1 = (choice) => {
+    setPlayer1(choice);
+  };
+
+  const cambiarOpcionJugador2 = (choice) => {
+    setPlayer2(choice);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='contenedor'>
+      <div className='flex'>
+        <div>
+          <p>Jugador 1: {player1} </p>
+          <button onClick={() => cambiarOpcionJugador1('piedra')}>Piedra</button>
+          <button onClick={() => cambiarOpcionJugador1('papel')}>Papel</button>
+          <button onClick={() => cambiarOpcionJugador1('tijera')}>Tijera</button>
+        </div>
+        <div>
+          <p>Jugador 2: {player2}</p>
+          <button onClick={() => cambiarOpcionJugador2('piedra')}>Piedra</button>
+          <button onClick={() => cambiarOpcionJugador2('papel')}>Papel</button>
+          <button onClick={() => cambiarOpcionJugador2('tijera')}>Tijera</button>
+        </div>
+        <button onClick={playGame}>Jugar</button>
+        {result && <p>Resultado: {result}</p>}
+      </div>
     </div>
   );
 }
